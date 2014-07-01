4f21bd8d9ef618d78f9f54dc9667d1422e18dbce
Michael Miller <Mmil09@gmail.com>
2014-07-01 20:54:13 +0000

    modified todo list validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -26,4 +26,32 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is what I'm doing today.")
   end
+  
+  it "displays an error when the todo list has a title fewer than 3 characters" do
+    expect(TodoList.count).to eq(0)
+  
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is what I do today"
+    click_button "Create Todo list"
+    
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+    
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+  
+  it "displays an error when no description" do
+    expect(TodoList.count).to eq(0)
+  
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is what I do today"
+    click_button "Create Todo list"
+    
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+    
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
 end
